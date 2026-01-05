30d05f2ab8d29d13d40be2f0b939c6b1b2dc3864
NAK Dipto <89862876+nak-dipto@users.noreply.github.com>
2026-01-06 02:33:28 +0600

    delete redundant files

--- server/server/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { uploadImage, upload } from '../controllers/uploadController.js';
-import { protect } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-
-router.post('/', protect, upload.single('image'), uploadImage);
-
-export default router;
